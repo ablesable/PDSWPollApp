022dff504252f95b98fd7af7867a3dba565ac000
ablesable <sobolbartosz@gmail.com>
2021-06-21 19:39:56 +0200

    Initial commit on extended branch

--- pdsw_poll_project/users/views.py
+++ pdsw_poll_project/users/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
 from users.forms import UserForm
-
-# Create your views here.
-
-# def register(request):
-#     form = UserCreationForm()
-#     return render(request, 'users/register.html', {'form': form})
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
             return redirect('home')
     else:
         form = UserForm()
