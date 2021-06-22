302cb63bcbb718887aa9623487a08a7dadf6a4ff
ablesable <sobolbartosz@gmail.com>
2021-06-22 03:48:31 +0200

    Basic voting, counting total, without age groups and allowing vote just once yet

--- pdsw_poll_project/poll/views.py
+++ pdsw_poll_project/poll/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .forms import CreatePollForm
 from .models import Poll
 # from django.contrib.auth.decorators import user_passes_test
@@ -18,6 +19,7 @@ def create(request):
             if form.is_valid():
                 
                 form.save()
+                messages.info(request, "Your poll has been created!")
                 return redirect('home')
         else:
             form = CreatePollForm()
@@ -31,17 +33,42 @@ def create(request):
 
 def results(request, poll_id):
     if request.user.is_superuser:
-        context = {}
+        poll = Poll.objects.get(pk=poll_id)
+        context = {
+            'poll' : poll
+        }
         return render(request, 'poll/results.html', context)
     else:
         messages.warning(request, "Unfoturnately - this feature is only for admin!")
         return redirect('home') 
 
 def vote(request, poll_id):
-    poll = Poll.objects.get(pk=poll_id)
-    if request.method == 'POST':
-        print(request.POST[''])
-    context = {
-        'poll': poll
-    }
-    return render(request, 'poll/vote.html', context)
+    if request.user.is_superuser:
+        messages.info(request, "Not this time - This feature is only for users. Please register first as one!")
+        return redirect('home') 
+    else:
+        poll = Poll.objects.get(pk=poll_id)
+        if request.method == 'POST':
+            selected_option = request.POST['poll']
+            print(request.POST['poll'])
+            
+            if selected_option == 'option1':
+                poll.first_option_count += 1
+            
+            elif selected_option == 'option2':
+                poll.second_option_count += 1 
+
+            elif selected_option == 'option3':
+                poll.third_option_count += 1
+            
+            else:
+                return HttpResponse(400)
+
+            poll.save()
+            return redirect('results', poll.id)
+
+            
+        context = {
+            'poll': poll
+        }
+        return render(request, 'poll/vote.html', context)
